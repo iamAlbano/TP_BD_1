23275b06ed1a6c772010cb0ff629c828a8a3005c
Guilherme Albano <42918659+iamAlbano@users.noreply.github.com>
2021-08-19 20:10:34 -0300

    fixes fixes fixes...

--- app/Views/templates/post.php
+++ app/Views/templates/post.php
@@ -3,14 +3,14 @@
     <div class="my-3 p-3 bg-white rounded box-shadow post feed-elements">
       <?php if ($post['id_user'] == session()->get('id')) : ?>
         <div class="media text-muted pt-3" style="padding: 0!important;">
-          <a href="Post/delete_post?id=<?= $post['id'] ?>&id_user=<?= $post['id_user']?>" class="delete-btn-link"><span class="fas fa-trash-alt delete-post" style="float: right"></span></a>
+          <a href="/TP_BD_1/public/Post/delete_post?id=<?= $post['id'] ?>&id_user=<?= $post['id_user']?>" class="delete-btn-link"><span class="fas fa-trash-alt delete-post" style="float: right"></span></a>
         </div>
       <?php endif; ?>
         
       <div class="media text-muted pt-3">
         <span style="float: right;margin-right: 20px;"> <?= $post['date'] ?></span>  
       
-        <a class="post-title-feed text-muted " href="Post?id=<?= $post['id'] ?>">
+        <a class="post-title-feed text-muted " href="/TP_BD_1/public/Post?id=<?= $post['id'] ?>">
         
           <img src="https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png" alt="mdo" width="32" height="32" class="rounded-circle">
           <div class="user-data">
@@ -26,7 +26,7 @@
       </div>
       <div class="row new-post-section-row" style="margin-bottom: 1rem;">
         <div class="col-10" >
-        <a  href="Post?id=<?= $post['id'] ?>" style="float:right;">continuar lendo...</a>
+        <a  href="/TP_BD_1/public/Post?id=<?= $post['id'] ?>" style="float:right;">continuar lendo...</a>
         </div>
       </div>
   
