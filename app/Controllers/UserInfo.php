c29762551aad32a9401890911fbbccb5d1875f17
Guilherme Albano <42918659+iamAlbano@users.noreply.github.com>
2021-08-19 13:49:46 -0300

    user info change [work in progress]

--- app/Controllers/UserInfo.php
+++ app/Controllers/UserInfo.php
@@ -22,16 +22,16 @@ class UserInfo extends BaseController
 	public function username()
   {
 	$this->isLoggedIn();
+    $this->myData();
 	echo view('templates/message', ['message' => 'Nome de usuário indisponível!', 'type' => 'warning']);
-    $this->newUser();
-  }
+    }
 
   public function email()
-  {
+    {
 	$this->isLoggedIn();
+    $this->myData();
 	echo view('templates/message', ['message' => 'E-mail já cadastrado!', 'type' => 'warning']);
-    $this->newUser();
-  }
+    }
 
 	public function isLoggedIn(){
 		if(!session()->has('name')){
@@ -40,16 +40,17 @@ class UserInfo extends BaseController
 	}
 
 	public function myData(){
-		$data= $this->viewData();
+		$data = $this->viewData();
+		
 		$userInfo = [
 			'name' => $data[0]->first_name,
 			'last_name' => $data[0]->last_name,
 			'email' => $data[0]->email,
-			'username' => session()->name,
+			'username' => session()->username,
 		  ];
+
 		echo view('templates/html_header');
         echo view('templates/navbar');
-		
 		echo view('templates/UserInformation', ['user' => $userInfo]);
 	}
 
@@ -57,37 +58,73 @@ class UserInfo extends BaseController
 
 		$db = DB::connect();
 
-		$parameter = ['username' => session()->name];
-
-		
+		$parameter = ['username' => session()->username];
 
 		$data = $db->query("SELECT * FROM tb_user WHERE username = :username:", $parameter)->getResultObject();
 		
-		// foreach ($data->getResult() as $row){
-			
-		// 	if($row->email == $parameter['email']){
-		// 		header("Location: email");
-		// 		die();
-				
-		// 	} 
-			
-		// 	if($row->username == $parameter['username']){
-		// 		header("Location: username");
-		// 		die();
-			
-		// 	}
-			
-		// }
-
 		$db->close();
 
-	
-		return $data;
-
-		// $this->log($parameter['name'], $parameter['username']);
-		
+		return $data;	
         
     }
+
+	public function changeData(){
+
+		$user = $this->viewData();
+
+		$db = DB::connect();
+    	$parameter = [
+		'id' => $user[0]->id,
+		'first_name' => $this->request->getPost('firstName'),
+		'last_name' => $this->request->getPost('lastName'),
+     	 'email' => $this->request->getPost('email'),
+      	 'username' => $this->request->getPost('username')
+   		 ];
+		
+
+		$data = $db->query("SELECT * FROM tb_user WHERE email = :email: OR username  = :username:", $parameter);
+		
+		
+
+		foreach ($data->getResult() as $row){
+			
+			if(strcmp($row->email,$parameter['email']) && !strcmp($row->email, $user[0]->email) ){
+				header("Location: email");
+				die();
+				
+			} 
+			
+			if(strcmp($row->username, $parameter['username']) && !strcmp($row->username, $user[0]->username)){
+				header("Location: username");
+				die();
+			
+			}
+			
+		}
+
+		$newData = $db->query("UPDATE `tb_user`
+		SET `first_name` =  :first_name:,
+		 	`last_name` = :last_name:,
+			 `email` = :email:,
+		 	`username` = :username:
+		WHERE `id` = :id:", $parameter);
+
+
+		$db->close();
+		$this->update($parameter['first_name'], $parameter['username']);
+		header("Location: /TP_BD_1/public/UserInfo/myData");
+		die();
+
+
+	}
+
+
+	public function update($name, $username){
+		session()->set('name', $name);
+		session()->set('username',  $username);
+	
+	}
+
 
 
 	public function log($name, $username){
--- app/Views/templates/UserInformation.php
+++ app/Views/templates/UserInformation.php
@@ -9,11 +9,11 @@
       
       <div class="col-md-7 col-lg-8 ">
         <h4 class="mb-3">Meus dados</h4>
-        <form class="needs-validation" novalidate="">
+        <form action="changeData" class="needs-validation" method="post" accept-charset="utf-8">
           <div class="row g-3">
             <div class="col-sm-6">
               <label for="firstName" class="form-label">Nome</label>
-              <input type="text" class="form-control" id="firstName" placeholder="<?=$user['name']?>" value="<?=$user['name']?>" required style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+              <input type="text" class="form-control" id="firstName" name="firstName" placeholder="<?=$user['name']?>" value="<?=$user['name']?>" required style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
               <div class="invalid-feedback">
                 Valid first name is required.
               </div>
@@ -21,7 +21,7 @@
 
             <div class="col-sm-6">
               <label for="lastName" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="<?=$user['last_name']?>" value="<?=$user['last_name']?>" required>
+              <input type="text" class="form-control" id="lastName" name="lastName" placeholder="<?=$user['last_name']?>" value="<?=$user['last_name']?>" required>
               <div class="invalid-feedback">
                 Valid last name is required.
               </div>
@@ -31,7 +31,7 @@
               <label for="username" class="form-label">Username</label>
               <div class="input-group has-validation">
                 <span class="input-group-text">@</span>
-                <input type="text" class="form-control" id="username"  value="<?=$user['username']?>" placeholder="<?=$user['username']?>" required>
+                <input type="text" class="form-control" id="username" name="username"  value="<?=$user['username']?>" placeholder="<?=$user['username']?>" required>
               <div class="invalid-feedback">
                   Your username is required.
                 </div>
@@ -40,7 +40,7 @@
 
             <div class="col-12">
               <label for="email" class="form-label">Email</label>
-              <input type="email" class="form-control" id="email" placeholder="<?=$user['email']?>" value="<?=$user['email']?>" required>
+              <input type="email" class="form-control" id="email" name="email" placeholder="<?=$user['email']?>" value="<?=$user['email']?>" required>
               <div class="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
